e5b6004e18cbd6b146fa930e6b5bd3c329899483
Milyra35 <marie.richir35@gmail.com>
2023-08-24 17:17:08 +0200

    Adding data to the homepage

--- controllers/AbstractController.php
+++ controllers/AbstractController.php
@@ -1,9 +1,10 @@
 <?php
 
 abstract class AbstractController {
-    public function render(string $view, array $values, string $file = "users") : void
+    public function render(string $view, string $title, array $values, string $file = "users") : void
     {
         $template = $view;
+        $pageName = $title;
         $folder = $file;
         $data = $values;
         require "./templates/$folder/layout.phtml";
--- controllers/StaticPageController.php
+++ controllers/StaticPageController.php
@@ -4,25 +4,25 @@ class StaticPageController extends AbstractController {
     // Homepage
     public function renderHomepage()
     {
-        $this->render("staticpages/homepage.phtml", []);
+        $this->render("staticpages/homepage.phtml", "Homepage", []);
     }
 
     // Confidentiality
     public function renderConfidentiality()
     {
-        $this->render("staticpages/confidentiality.phtml", []);
+        $this->render("staticpages/confidentiality.phtml", "Confidentiality", []);
     }
 
     // Legal-notices
     public function renderLegalNotices()
     {
-        $this->render("staticpages/legal-notices.phtml", []);
+        $this->render("staticpages/legal-notices.phtml", "Legal Notices", []);
     }
 
     // Credits
     public function renderCredits()
     {
-        $this->render("staticpages/credits.phtml", []);
+        $this->render("staticpages/credits.phtml", "Credits", []);
     }
 }
 
