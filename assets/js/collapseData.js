0589529e7a0a04e7718cbbc5887a28bfbbbbcb7c
Milyra35 <marie.richir35@gmail.com>
2023-08-28 16:23:32 +0200

    Trying to collapse the villagers' data

--- assets/js/collapseData.js
+++ assets/js/collapseData.js
@@ -1,21 +1,36 @@
 function collapseData() 
 {
-    let title = document.getElementById('villager-events');
-    let events = document.getElementById('events');
-    events.style.display = 'none';
+    let title = document.querySelectorAll('.title');
+    let events = document.querySelectorAll('.events');
 
-    title.addEventListener("click",function() {
-        let isHidden = events.style.display === 'none';
+    for(let i=0; i<title.length; i++)
+    {
+        title[i].addEventListener("click", function() {
 
-        if(isHidden)
-        {
-            events.style.display = 'table';
-        }
-        else
-        {
-            events.style.display = 'none';
-        }
-    });
+            if(events[i].style.display === 'none' || events[i].style.display === '')
+            {
+                events[i].style.display = 'table';
+            }
+            else
+            {
+                events[i].style.display = 'none';
+            }
+        })
+    }
+    // events.style.display = 'none';
+
+    // title.addEventListener("click",function() {
+    //     let isHidden = events.style.display === 'none';
+
+    //     if(isHidden)
+    //     {
+    //         events.style.display = 'table';
+    //     }
+    //     else
+    //     {
+    //         events.style.display = 'none';
+    //     }
+    // });
 }
 
 export { collapseData };
