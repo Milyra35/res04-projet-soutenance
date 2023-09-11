a774a2ecba4c941c9343e75fda70699fd0aa8a29
Milyra35 <marie.richir35@gmail.com>
2023-09-11 15:18:00 +0200

    Working on the css on the single villager and changed some things on the collapseData function JS

--- assets/js/collapseData.js
+++ assets/js/collapseData.js
@@ -2,6 +2,7 @@ function collapseData()
 {
     let title = document.querySelectorAll('.title');
     let events = document.querySelectorAll('.events');
+    let button = document.querySelectorAll('.title button');
 
     for(let i=0; i<title.length; i++)
     {
@@ -12,10 +13,12 @@ function collapseData()
             if(events[i].style.display === 'none' || events[i].style.display === '')
             {
                 events[i].style.display = 'table';
+                button[i].classList.add('button-focus');
             }
             else
             {
                 events[i].style.display = 'none';
+                button[i].classList.remove('button-focus');
             }
         })
     }
