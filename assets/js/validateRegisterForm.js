b5ab86ca7a23fbea3ecd7d3a1c7d01968877aa00
Milyra35 <marie.richir35@gmail.com>
2023-10-02 11:25:09 +0200

    Fixed an issue on the validateRegisterForm

--- assets/js/validateRegisterForm.js
+++ assets/js/validateRegisterForm.js
@@ -16,41 +16,69 @@ function validateRegisterForm()
         let user = new User(username, email, password, confirmPassword);
         let validate = user.validate();
 
-        let formData = new FormData();
-        formData.append('username', username);
-        formData.append('email', email);
-        formData.append('password', password);
-        formData.append('confirm-password', confirmPassword);
-        formData.append('submit-new-user', true);
+        
 
-        let options = {
-            method: 'POST',
-            body: formData
-        };
+        if(!validate)
+        {
+            isValid = false;
+        }
+        else
+        {
+            let formData = new FormData();
+            formData.append('username', username);
+            formData.append('email', email);
+            formData.append('password', password);
+            formData.append('confirm-password', confirmPassword);
+            formData.append('submit-new-user', true);
 
-        fetch('/res04-projet-soutenance/register', options)
-        .then(response => response.json())
-        .then(data => {
-            if(data.exists)
-            {
-                let errorUsername = document.querySelector('.errorUsername');
-                errorUsername.textContent = "This username already exists";
-                isValid = false;
-            }
-            else if(!validate)
-            {
-                isValid = false;
-            }
-            else
-            {
-                isValid = true;
-                registerForm.submit();
-                window.location.href = "/res04-projet-soutenance/login";
-            }
-        })
-        .catch(error => {
-            console.error('An error occured: ' + error);
-        })
+            let options = {
+                method: 'POST',
+                body: formData
+            };
+
+            fetch('/res04-projet-soutenance/register', options)
+            .then(response => response.json())
+            .then(data => {
+                if(data.exists)
+                {
+                    let errorUsername = document.querySelector('.errorUsername');
+                    errorUsername.textContent = "This username already exists";
+                    isValid = false;
+                }
+                else
+                {
+                    isValid = true;
+                    registerForm.submit();
+                    window.location.href = "/res04-projet-soutenance/login";
+                }
+            })
+            .catch(error => {
+                console.error('An error occured: ' + error);
+            })
+        }
+
+        // fetch('/res04-projet-soutenance/register', options)
+        // .then(response => response.json())
+        // .then(data => {
+        //     if(data.exists || !validate)
+        //     {
+        //         if(data.exists)
+        //         {
+        //             let errorUsername = document.querySelector('.errorUsername');
+        //             errorUsername.textContent = "This username already exists";
+        //         }
+        //         isValid = false;
+        //     }
+        //     else
+        //     {
+        //         isValid = true;
+        //         registerForm.submit();
+        //         window.location.href = "/res04-projet-soutenance/login";
+        //     }
+        // })
+        // .catch(error => {
+        //     console.error('An error occured: ' + error);
+        // })
     });
 }
 
