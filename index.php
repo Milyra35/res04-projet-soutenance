be9cf1ad77bea6765818958def948669bb232c86
Milyra35 <marie.richir35@gmail.com>
2023-10-04 07:26:55 +0000

    Fixed a css issue on the transition between light and dark mode

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // L'IDE limite la taille des fichiers à télécharger à 2Mo, et ce n'était pas possible de le changer
 // La taille des fichiers de sauvegarde font en moyenne 6Mo donc l'upload ne fonctionne pas sur l'IDE
-// J'ai du faire sur mon serveur local
+// J'ai du faire sur mon serveur local mais le reste est fonctionnel
 
 require 'services/autoload.php';
 
